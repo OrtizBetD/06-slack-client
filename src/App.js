85895e2cc5627d8849e8a2b47747f1e861732309
Orphe650 <davortiz@gmail.com>
2020-02-28 08:57:22 +0100

    Routes part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
-import Chat from './Chat'
-import './styles/App.css'
+import React, { Component } from "react";
+import Chat from "./Chat";
+import "./styles/App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Signup from "./Signup";
+import Login from "./Login";
 
 class App extends Component {
-	// Methods
-	// Render
-	render() {
-		return <Chat />
-	}
+  // Methods
+  // Render
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Chat} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
