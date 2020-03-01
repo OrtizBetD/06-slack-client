bb15b5416fcaacd8f0a74408f0e62e5075d890db
Orphe650 <davortiz@gmail.com>
2020-03-01 09:00:25 +0100

    login

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,69 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
 class Login extends React.Component {
-	// Data
-	state = {}
-	// Methods
-	login = () => {}
-	// Render
-	render() {
-		return (
-			<form className="card" onSubmit={this.login}>
-				<input type="text" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button type="submit" className="positive">
-					Login
-				</button>
-				<div className="link">
-					<a href="/signup">New here? Create an account</a>
-				</div>
-				<div className="error">{this.state.error}</div>
-			</form>
-		)
-	}
+  // Data
+  state = {
+    email: "",
+    password: "",
+    error: ""
+  };
+  // Methods
+  login = e => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API}/users/login`, {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        if (response.data === "email not found") {
+          this.setState({
+            error: "Email not found"
+          });
+        } else if (response.data === "invalid password") {
+          this.setState({
+            error: "invalid password"
+          });
+        } else {
+          localStorage.setItem("token", response.data);
+          this.props.history.push("/");
+        }
+      });
+  };
+  // Render
+  render() {
+    return (
+      <form className="card" onSubmit={this.login}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={e =>
+            this.setState({
+              email: e.target.value
+            })
+          }
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={e =>
+            this.setState({
+              password: e.target.value
+            })
+          }
+        />
+        <button type="submit" className="positive">
+          Login
+        </button>
+        <div className="link">
+          <a href="/signup">New here? Create an account</a>
+        </div>
+        <div className="error">{this.state.error}</div>
+      </form>
+    );
+  }
 }
 
-export default Login
+export default Login;
